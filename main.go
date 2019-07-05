cefb31edf05d5b019838c2590ec224c5ad79a6b2
yasar budulgan <yb.budulgan@gmail.com>
2019-07-05 23:40:52 +0300

    Read CSV file and print problems

--- main.go
+++ main.go
@@ -1,33 +1,49 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
 // DefaultFile indicates the default filepath containing problems.
 // If user specifies a different filepath via arguments, this will be overriden.
-const DefaultFile = "./problems2.csv"
+const DefaultFile = "./problems.csv"
 
-var filepath string
+var filePath string
 
 func main() {
 
 	parseArguments()
-	fmt.Printf("Filepath: %s\n", filepath)
+
+	f, _ := os.Open(filePath)
+	r := csv.NewReader(bufio.NewReader(f))
+
+	for {
+		record, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Println(record)
+	}
 
 }
 
 func parseArguments() {
-	flag.StringVar(&filepath, "file", "", "path of CSV file containing problems. (default: \"./problems.csv\")")
+	flag.StringVar(&filePath, "file", "", "path of CSV file containing problems. (default: \"./problems.csv\")")
 	flag.Parse()
 
-	if filepath == "" {
+	if filePath == "" {
 		if _, err := os.Stat(DefaultFile); os.IsNotExist(err) {
 			fmt.Fprintln(os.Stderr, "Error: Problems file couldn't be found. Filepath must be specified")
 			flag.PrintDefaults()
 			os.Exit(1)
 		}
+		filePath = DefaultFile
 	}
 }
